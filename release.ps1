ca1e5e6f07ff4e6d846ed70ff57740058dbfb1c3
VulpesStella <81358657+VulpesStella@users.noreply.github.com>
2026-09-02 00:38:18 +0800

    Revert "fix: pwsh"
    This reverts commit bcaee4411b5a467d19816b67b6ff152d835a5da1.

--- release.ps1
+++ release.ps1
@@ -75,7 +75,7 @@ try {
     if ($hasChanges) {
         Write-Host "[WARN] Found uncommitted changes. Committing and pushing..." -ForegroundColor Yellow
         git add .
-        git commit -m "chore: bump version to $tagName"
+        git commit -m "chore: bump version to $tagName [skip ci]"
         
         Write-Host "[INFO] Pushing commits to remote repository..." -ForegroundColor Cyan
         git push origin (git branch --show-current)
